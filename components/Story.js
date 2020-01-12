38e42582a963793d3077a1fa6004b2ca73d5eb86
Bousine <asif21hasan@gmail.com>
2020-01-12 14:49:17 -0500

    Added state management functions and favorite button listener to handle favorites

--- components/Story.js
+++ components/Story.js
@@ -14,9 +14,9 @@ export default function Story(story){
           <a href="#/item?id=${story.id}">
             ${story.comments_count} comments
           </a>
-          <span class="favorite">
+          <span class="favorite" data-story='${JSON.stringify(story)}'>
             <img class="heart" src="https://icon.now.sh/heart/ccc">
-            Add to Favorites
+            ${story.isFavorite ? "Remove from Favorites" : "Add to Favorites"}
           </span>
         </div>
       </div>
